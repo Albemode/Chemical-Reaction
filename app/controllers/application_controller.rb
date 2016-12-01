cd44cd9711f4a9f246575a2a4267d8260b03de68
Alberto Modesto <albe.mode.24@gmail.com>
2016-12-01 12:12:27 -0500

    adding forms for db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  class Headline
+  	def initialize(title, link)
+  		@title = title
+  		@link = link
+  	end
+  	attr_reader :title
+  	attr_reader :link
+  end
+
+  # Pull the site
+  def scrape_chinadaily
+  	require 'open-uri'
+  	doc = Nokogiri::HTML(open("http://www.chinadaily.com.cn/entertainment/celebrities.html"))
+
+  	# Parse for headlines and create array
+  	headlines = doc.css('ul.chinanewslst>li')
+  	@headlinesArray = []
+  	headlines.each do |h|
+  		title = h.css('h2.titlebox>a').text
+  		link = h.css('h2.titlebox>a')[0]['href']
+  		@headlinesArray << Headline.new(title, link)
+  	end
+
+  	render template: 'scrape_chinadaily'
+  end
 end
